ae4e55e98b06e449e3026eb6942354013c60b5fc
James Sharpe <jameswrightsharpe@gmail.com>
2019-07-31 23:02:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ function takeANumber(currentLine, customerName){
 }
 
 function nowServing(currentLine){
-  if(currentLine.length === 0){return "There is nobody waiting to be served!"}
+  if(currentLine.length === 0){return "There is nobody waiting to be served!"};
   return `Currently serving ${currentLine.shift()}.`;
 }
 
 function currentLine(currentLine){
-  if(currentLine.length === 0){return "The line is currently empty."}
-  var message = `The line is currently: `
+  if(currentLine.length === 0){return "The line is currently empty."};
+  var message = `The line is currently: `;
   for(var i = 0; i < currentLine.length; i++){
     message += `${i+1}. ${currentLine[i]}, `
   }
   return message.slice(0,-2)
-  
 }
